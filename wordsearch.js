4b4f74b90a15eca7e19f893ea16f3725c96c0c48
Calvycoco <su.calvin@live.com>
2023-08-02 18:49:53 -0700

    pair programing with Noah and Hinali

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+ 
+  let arr = [];
+  for (let col = 0; col < letters.length; col++) {
+    let temp = [];
+    for (let row = 0; row < letters.length; row++) {
+      temp.push(letters[row][col]);
     }
-}
-
-module.exports = wordSearch
+    arr.push(temp);
+  }
+  
+  let horizontal = hasWord(letters, word);
+  let vertical = hasWord(arr, word);
+  
+  if (horizontal || vertical) {
+    return true;
+  }
+  return false;
+  
+};
+  
+const hasWord = (arr, word) => {
+  const join = arr.map(ls => ls.join(''));
+  for (l of join) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+  
+module.exports = wordSearch;
